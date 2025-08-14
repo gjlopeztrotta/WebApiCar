4c3a4544dda000020cb1eccb17e873be9a761ed2
gjlopeztrotta <gjlopeztrotta@gmail.com>
2025-08-14 23:40:59 +0200

    14082025

--- WebApiCar/obj/Release/net8.0/WebApiCar.AssemblyInfo.cs
+++ WebApiCar/obj/Release/net8.0/WebApiCar.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiCar")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b434fd672571cf8d90a47349f395652dc4be7b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiCar")]
